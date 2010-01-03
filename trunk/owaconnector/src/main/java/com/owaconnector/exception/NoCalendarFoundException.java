08887dd6837186208cdd3d8b7b2590371a16d8a8
b.walet <b.walet@1537e20c-f544-11de-a799-c55347f9a4a0>
2010-01-03 12:09:32 +0000

    Rebuilt using Spring Roo

---
+++ trunk/owaconnector/src/main/java/com/owaconnector/exception/NoCalendarFoundException.java
@@ -0,0 +1,11 @@
+package com.owaconnector.exception;
+
+public class NoCalendarFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4803320163258133176L;
+
+	
+}
