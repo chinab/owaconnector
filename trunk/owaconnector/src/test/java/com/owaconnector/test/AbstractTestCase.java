96641007e9b9acec4a5c951c2c840fd25553ff6b
b.walet <b.walet@1537e20c-f544-11de-a799-c55347f9a4a0>
2010-01-03 13:06:20 +0000

    fixed testcase for PasswordService

--- trunk/owaconnector/src/test/java/com/owaconnector/test/AbstractTestCase.java
+++ trunk/owaconnector/src/test/java/com/owaconnector/test/AbstractTestCase.java
@@ -2,14 +2,16 @@ package com.owaconnector.test;
 
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath*:META-INF/spring/applicationContext.xml"})
-@TransactionConfiguration(transactionManager="transactionManager", defaultRollback=false)
-
-public class AbstractTestCase extends AbstractTransactionalJUnit4SpringContextTests{
+@ContextConfiguration(locations = {
+		"classpath*:META-INF/spring/applicationContext.xml",
+		"classpath*:WEB-INF/spring/webmvc-config.xml" })
+//@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)
+public class AbstractTestCase {
 
 }
