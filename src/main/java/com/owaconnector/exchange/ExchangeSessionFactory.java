2d729f8a8bd163009b87e023c77a7072c5df6dfb
b.walet <b.walet@1537e20c-f544-11de-a799-c55347f9a4a0>
2010-03-01 20:46:31 +0000

    import davmail

--- src/main/java/com/owaconnector/exchange/ExchangeSessionFactory.java
+++ src/main/java/com/owaconnector/exchange/ExchangeSessionFactory.java
@@ -1,29 +1,19 @@
 package com.owaconnector.exchange;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 
-import org.apache.http.HttpException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.owaconnector.exception.AuthenticationFailedException;
-import com.owaconnector.exception.UnknownHttpStatusException;
+import davmail.exchange.ExchangeSession;
 
 @Component
 public class ExchangeSessionFactory {
 
-	@Autowired
-	private ClientFacade facade;
+	public ExchangeSession getInstance(String url, String username,
+			String password) throws IOException {
 
-	@Autowired
-	private ExchangeProperties props;
+		return davmail.exchange.ExchangeSessionFactory.getInstance(url,
+				username, password);
 
-	public ExchangeSession getInstance(String url, String username, String password)
-			throws IOException, AuthenticationFailedException, UnknownHttpStatusException,
-			HttpException, URISyntaxException {
-		ExchangeSession session = new ExchangeSession(facade, props);
-		session.login(url, username, password);
-		return session;
 	}
 }
